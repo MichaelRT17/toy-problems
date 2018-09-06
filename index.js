8dc762957616be30d9a2923243a693ee32ea1d77
Michael Thurman <michaelrt17@icloud.com>
2018-09-06 11:19:13 -0600

    completed toy problems spinWords and moveZeros

--- index.js
+++ index.js
@@ -108,17 +108,57 @@ const whosOnline = (friends) => {
 whosOnline(
     [
         {
-          username: 'David',
-          status: 'online',
-          lastActivity: 10
+            username: 'David',
+            status: 'online',
+            lastActivity: 10
         }, {
-          username: 'Lucy', 
-          status: 'offline',
-          lastActivity: 22
+            username: 'Lucy',
+            status: 'offline',
+            lastActivity: 22
         }, {
-          username: 'Bob', 
-          status: 'online',
-          lastActivity: 104
+            username: 'Bob',
+            status: 'online',
+            lastActivity: 104
         }
     ]
 )
+
+// Write a function that takes in a string of one or more words, and returns the same string, 
+// but with all five or more letter words reversed (Just like the name of this Kata). 
+// Strings passed in will consist of only letters and spaces. Spaces will be included only when more than one word is present.
+
+
+// Examples:
+
+// spinWords( "Hey fellow warriors" ) => returns "Hey wollef sroirraw" 
+// spinWords( "This is a test") => returns "This is a test" 
+// spinWords( "This is another test" )=> returns "This is rehtona test"
+
+function spinWords(str) {
+    return str.split(' ').map(word => {
+        if (word.length >= 5) {
+            return word.split('').reverse().join('')
+        }
+        return word
+    }).join(' ');
+}
+
+// Below should return 'Hey wollef sroirraw'
+
+spinWords('Hey fellow warriors')
+
+
+// Write an algorithm that takes an array and moves all of the zeros to the end,
+// preserving the order of the other elements.
+
+//  moveZeros([false,1,0,1,2,0,1,3,"a"]) // returns[false,1,1,2,1,3,"a",0,0]
+
+var moveZeros = function (arr) {
+    let zeros = arr.filter(x => x === 0)
+    let others = arr.filter(y => y !== 0)
+    return others.concat(zeros)
+}
+
+// Below should return [true, 5, "hello", 6, 3, "z", 0, 0, 0]
+
+moveZeros([true, 0, 5, "hello", 0, 6, 3, 0, "z"])
